8055fe76a5966282e6622c1f57df5d88a886fa8d
Kevin <kevin.rodriguez678@gmail.com>
2022-06-14 21:33:16 -0400

    add data folder

---
+++ data/students-data.js
@@ -0,0 +1,16 @@
+const students = [
+  {first: 'Deivid', last: 'Rodriguez', grade: 12},
+  {first: 'William', last: 'Rodriguez', grade: 6},
+  {first: 'Ulgbeck', last: 'Ismilov', grade: 10},
+  {first: 'Josue', last: 'Rodriguez', grade: 2},
+  {first: 'Luis', last: 'Gonzalez', grade: 12},
+  {first: 'Jesus', last: 'Morales', grade: 7},
+  {first: 'Gorge', last: 'Mendez', grade: 1},
+  {first: 'Adrian', last: 'Gomez', grade: 12},
+  {first: 'Zlatan', last: 'Ibrahimovic', grade: 99},
+  {first: 'Chris', last: 'Ellingsworth', grade: 10},
+]
+
+export{
+  students
+}
